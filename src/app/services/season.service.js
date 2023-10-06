51b94a05714b3442e9d045172628f03ee76050e2
Irtiza Hassan <irtizahassanbvh@gmail.com>
2023-10-07 01:44:44 +0500

    season edit code

--- src/app/services/season.service.js
+++ src/app/services/season.service.js
@@ -2,12 +2,19 @@ import http from './http-common';
 
 class SeasonService {
   getAll() {
-    return http.get('/season');
+    return http.get('/season/list');
+  }
+  getRowByID(rowID){
+    return http.get(`/season/${rowID}`)
   }
   
+  updateData(data) {
+    console.log(data)
+    return http.put('/season/edit',data);
+  }
   insertData(data) {
     console.log(data)
-    return http.post('/seasonAdd',data);
+    return http.post('/season/add',data);
   }
   
 }
